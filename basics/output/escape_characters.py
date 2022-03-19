fe2b51e0460e17ffc29b6985585cba613a4fce1b
voxxlight369 <101944708+voxxlight369@users.noreply.github.com>
2022-03-19 19:34:59 +0000

    "Added code to display escape characters."

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#Display escape characters
+print("\n Display a new line")
+print("\t Display a tab space")
+print("\\ Display a back slash")
+print("\" Display a double quote")
+print("\' Display a single quote")
